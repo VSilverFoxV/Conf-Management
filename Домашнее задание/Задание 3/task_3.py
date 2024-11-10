2f5772d845a7ed1477eb50ce67d4cf1d5d266bd4
VSilverFoxV <valentinadvel@gmail.com>
2024-11-10 20:08:11 +0300

    Добавлено правило распознавания и вычисления константных выражений

--- Домашнее задание/Задание 3/task_3.py
+++ Домашнее задание/Задание 3/task_3.py
@@ -1,10 +1,30 @@
 from peco.peco import *
 
+# Глобальный словарь для хранения значений переменных
+variables = {}
+
 # Обработка чисел, массивов, объектов (словарей) и строк
 mknum = to(lambda n: float(n))
 mkarr = to(lambda a: list(a))
 mkobj = to(lambda o: dict(o))
 mkstr = to(lambda s: str(s))
+
+# Обработка определения константных переменных
+def assign_const(n_v):
+    variables[n_v[0]] = n_v[1]
+    return n_v  # Возвращаем значение, чтобы оно добавилось в стек
+
+mkAssign = to(assign_const)
+
+# Обработка константных выражений
+def constRes(name):
+    # Получение значения переменной из глобального словаря
+    if name not in variables:
+        raise NameError(f"Variable '{name}' is not defined.")
+    
+    return variables[name]
+
+mkConstRes = to(constRes)
 
 # Обработка лишних пробелов, включая обработку комментариев
 ws = many(eat(r'\s+'))
@@ -27,13 +47,15 @@ array = seq(skip(r'\(list'), group(many(val)), skip(r'\)'), mkarr)
 
 item = group(seq(name, skip(r'=>'), val))
 
-const = group(seq(name, skip(r'='), val))
+const = seq(group(seq(name, skip(r'='), val)), mkAssign)
 
 obj = seq(skip(r'{'), group(many(seq(item, skip(r',')))), skip(r'}'), mkobj)
 
 string = seq(skip(r'@\"'), tok(r'[^"]*'), skip(r'\"'), mkstr)
 
-val = alt(num, string, array, obj)
+constExpr = seq(skip(r'#\['), name, skip(r'\]'), mkConstRes)
+
+val = alt(num, string, array, obj, constExpr)
 
 main = seq(group(many(const)), ws, mkobj)
 
@@ -46,6 +68,7 @@ def test():
         memory => 1024,
     }
     str = @"ASS"
+    test = #[a]
     '''
     s = parse(src, main)
     print(s.ok)
